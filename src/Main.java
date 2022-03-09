ff94226a74f58d7ce1f6ef818b7d36072c2e5f4a
4804W15 <4804W15@usa.miracosta.cc.ca.us>
2022-03-08 21:22:02 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public void swap(int[] array, int pos1, int pos2) {
+
+        int temp = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = temp;
+
+
+    }
+}
